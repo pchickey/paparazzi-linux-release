83bf5c8c3942708fb57962f3e854f99b951e3c3a
Pat Hickey <pat@moreproductive.org>
2009-12-29 21:43:43 -0500

    removed lots of garbage

--- conf/autopilot/linuxfc.h
+++ conf/autopilot/linuxfc.h
@@ -4,51 +4,6 @@
 #define CONFIG_LINUXFC_H
 
 /* Stub from tiny_2_1_1.h created Dec09 Pat Hickey */
-
-#ifdef SITL
-/* Dummy definitions: adc are unused anyway */
-#define AdcBank0(x) (x)
-#define AdcBank1(x) (x)
-#endif /* SITL */
-
-/* Master oscillator freq.       */
-#define FOSC (12000000) 
-
-/* PLL multiplier                */
-#define PLL_MUL (5)         
-
-/* CPU clock freq.               */
-#define CCLK (FOSC * PLL_MUL) 
-
-/* Peripheral bus speed mask 0x00->4, 0x01-> 1, 0x02 -> 2   */
-#define PBSD_BITS 0x00    
-#define PBSD_VAL 4
-
-/* Peripheral bus clock freq. */
-#define PCLK (CCLK / PBSD_VAL) 
-
-#define LED_1_BANK 1
-#define LED_1_PIN 17
-
-#define LED_2_BANK 1
-#define LED_2_PIN 16
-
-#define LED_3_BANK 1
-#define LED_3_PIN 23
-
-#define LED_4_BANK 1
-#define LED_4_PIN 18
-
-#define POWER_SWITCH_LED 4
-
-#define LED_5_BANK 1
-#define LED_5_PIN 22
-
-#define CAM_SWITCH_LED 5
-
-#define LED_GPS_RESET_BANK 1
-#define LED_GPS_RESET_PIN 21
-
 #define Configure_GPS_RESET_Pin() LED_INIT(GPS_RESET)
 #define Set_GPS_RESET_Pin_LOW() LED_ON(GPS_RESET)
 #define Open_GPS_RESET_Pin() ClearBit(LED_DIR(GPS_RESET), LED_PIN(GPS_RESET))
@@ -60,16 +15,6 @@
 #define SERVO_CLOCK_PINSEL_BIT 10
 /* p1.20          */
 #define SERVO_RESET_PIN 20
-
-/* PPM : rc rx on P0.6*/
-#define PPM_PINSEL PINSEL0
-#define PPM_PINSEL_VAL 0x02
-#define PPM_PINSEL_BIT 12
-#define PPM_CRI TIR_CR2I
-#define PPM_CCR_CRF TCCR_CR2_F
-#define PPM_CCR_CRR TCCR_CR2_R
-#define PPM_CCR_CRI TCCR_CR2_I
-#define PPM_CR T0CR2
 
 /* ADC */
 
