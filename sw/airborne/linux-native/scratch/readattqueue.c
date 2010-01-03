5c086767bf2a2e87f70d2940a3fe4975aef47a42
Pat Hickey <pat@moreproductive.org>
2010-01-02 21:32:14 -0500

    sends att info struct instead of message

---
+++ sw/airborne/linux-native/scratch/readattqueue.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <mqueue.h>
+#include <unistd.h>
+#include "queues.h"
+
+#define die(msg) { perror(msg); exit(EXIT_FAILURE); }
+
+void print_hex( unsigned char *, int);
+
+static void msgprinter( union sigval sv )
+{
+  struct mq_attr attr;
+  ssize_t nr;
+  void *buf;
+  mqd_t mqdes;
+  struct sigevent mq_callback;
+  struct att_msg *msg;
+
+//  printf("entered msgprinter\n");
+
+  mqdes = *((mqd_t *) sv.sival_ptr);
+  
+   if (mq_getattr(mqdes, &attr) == -1) die("mq_getattr");
+  buf = malloc(attr.mq_msgsize);
+    if (buf == NULL) die("malloc");
+  
+  do {
+    nr = mq_receive(mqdes, buf, attr.mq_msgsize, NULL);
+//    printf("> %d\n",nr);
+    msg = buf;  // dont need to cast
+    printf("got channel %d value %d\n", msg->channel, msg->value);
+  } while (nr > -1);
+  
+  free(buf);
+  
+//  printf("re-registering notify\n");
+  mq_callback.sigev_notify = SIGEV_THREAD;
+  mq_callback.sigev_notify_function = msgprinter;
+  mq_callback.sigev_notify_attributes = NULL;
+  mq_callback.sigev_value.sival_ptr = &mqdes;
+
+  if (mq_notify(mqdes, &mq_callback) == -1) die("mq_notify re-register");
+
+  printf("exiting msgprinter\n");
+}
+
+void print_hex(unsigned char* data, int len)
+{
+  int ii;
+  for (ii = 0; ii < len; ii++)
+    printf("%x ", data[ii]);
+  if (len > 0) printf("\n");
+}
+
+int main (void)
+{
+  printf("entering main\n");
+  mqd_t mqdes;
+  struct sigevent mq_callback;
+  
+  mqdes = mq_open(ATT_MQ_NAME, O_RDONLY | O_CREAT, 0666, NULL );
+  if (mqdes == (mqd_t) -1) die("mq_open");
+
+  printf("mq opened\n");
+
+  mq_callback.sigev_notify = SIGEV_THREAD;
+  mq_callback.sigev_notify_function = msgprinter;
+  mq_callback.sigev_notify_attributes = NULL;
+  mq_callback.sigev_value.sival_ptr = &mqdes;
+
+  if (mq_notify(mqdes, &mq_callback) == -1) die("mq_notify");
+
+  printf("pausing main\n");
+  pause();
+}
+
--- sw/airborne/linux-native/scratch/ttytest.c
+++ sw/airborne/linux-native/scratch/ttytest.c
@@ -16,6 +16,8 @@
 #define ATT_MODEM "/dev/ftdi1"
 
 void mq_send_gps(void*, void*, int);
+void mq_send_att(void*, void*, int);
+
 void print_callback(void*, void*, int);
 
 struct tty_reader_arguments
@@ -26,15 +28,40 @@ struct tty_reader_arguments
   void *cb_args;
 };
 
-struct mq_send_gps_args
+struct mq_send_args
 {
   mqd_t queue;
 };
 
 void mq_send_gps( void* v_args, void* data, int len )
 {
-  struct mq_send_gps_args* args = (struct mq_send_gps_args *) v_args;
+  struct mq_send_args* args = (struct mq_send_args *) v_args;
   mq_send(args->queue, data, len, 0);
+}
+
+void mq_send_att( void* v_args, void* data_v, int len )
+{
+  struct att_msg msg;
+  char * data = (char *) data_v;
+  struct mq_send_args* args = (struct mq_send_args *) v_args;
+
+  // expecting data to be a string like
+  // "$A0,254\r\n"
+  // or "$A1,21\r\n"
+  // where data[2] is the channel number,
+  // and data[4] begins the value
+  data[len] = 0;
+  printf("%s",data);
+  
+  msg.channel = atoi(&data[2]);
+  msg.value = atoi(&data[4]);
+
+  if (msg.channel > 0 && msg.channel <= 3)
+  {
+//    printf("parsed channel %d value %d\n", msg.channel, msg.value);
+    mq_send(args->queue,(char *)&msg,sizeof(msg), 0);
+//    printf("message sent\n");
+  }
 }
 
 void print_callback( void* nachtmicht, void* str_v, int len )
@@ -62,9 +89,9 @@ void *tty_reader( void *v_args )
 
   while(1)
   {
-    printf("%s---",args->fname);
+//    printf("%s---",args->fname);
     res = read(fd,buf,255);
-    printf(">%d\n", res);
+//    printf(">%d\n", res);
     if (res > -1)
     {
       args->callback(args->cb_args,buf,res);
@@ -80,33 +107,37 @@ void *tty_reader( void *v_args )
 
 int main( void )
 {
-  pthread_t threads[2];
+  pthread_t gps_thread, att_thread;
   struct tty_reader_arguments gps_args, att_args;
 
   mqd_t gps_queue = mq_open(GPS_MQ_NAME, O_WRONLY | O_CREAT, 0666, NULL);
-  mqd_t att_queue = mq_open(ATT_MQ_NAME, O_WRONLY | O_CREAT, 0666, NULL);
-  perror("a");
 
   bzero(&gps_args,sizeof(gps_args));
   gps_args.fname = GPS_MODEM;
   gps_args.tio.c_cflag = GPS_BAUD | CS8 | CREAD;
   gps_args.tio.c_lflag = ICANON;
   gps_args.callback = mq_send_gps;
- 
-  struct mq_send_gps_args gps_cbargs;
+  
+  struct mq_send_args gps_cbargs;
   gps_cbargs.queue = gps_queue;
- 
   gps_args.cb_args = &gps_cbargs; 
 
+  // pthread_create(&gps_thread, NULL, tty_reader, (void *)&gps_args);
+  
+  // setup attitude thread:
+
+  mqd_t att_queue = mq_open(ATT_MQ_NAME, O_WRONLY | O_CREAT, 0666, NULL);
+  
   bzero(&att_args,sizeof(att_args));
   att_args.fname = ATT_MODEM;
   att_args.tio.c_cflag = ATT_BAUD | CS8 | CREAD;
   att_args.tio.c_lflag = ICANON;
-  att_args.callback = print_callback;
-  att_args.cb_args = NULL;
+  att_args.callback = mq_send_att;
+  struct mq_send_args att_cbargs; 
+  gps_cbargs.queue = att_queue;
+  att_args.cb_args = &att_queue;
 
-//  pthread_create(&threads[1], NULL, tty_reader, (void *)&att_args);
-  pthread_create(&threads[0], NULL, tty_reader, (void *)&gps_args);
+  pthread_create(&att_thread, NULL, tty_reader, (void *)&att_args);
 
   pthread_exit(NULL);
 }
