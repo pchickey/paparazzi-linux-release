958c56e988f2d55bc26cda9131f5b9cb0e55c388
Pat Hickey <pat@moreproductive.org>
2010-01-17 19:17:23 -0500

    dont want to track var

--- var/LINUXFC/modules.c
+++
@@ -1,9 +0,0 @@
-/* This file has been generated from /home/pat/paparazzi3/conf/airframes/linuxfc.xml */
-/* Please DO NOT EDIT */
-
-#include "modules.h"
-
-#ifdef USE_MODULES
-
-
-#endif // USE_MODULES
